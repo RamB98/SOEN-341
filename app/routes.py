35cb86f68ff61b750f15c194b8599aa4478713ff
Samson Kaller <samson.kaller@gmail.com>
2021-10-13 16:06:38 -0400

    Added Basic Registration/Login Pages (front end)

--- app/routes.py
+++ app/routes.py
@@ -2,5 +2,14 @@ from flask import render_template, url_for, flash, redirect, request, abort
 from . import app
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template('Home.html')
+
+@app.route("/login")
+def login_page():
+    return render_template('Login.html');
+
+@app.route("/register")
+def register_page():
+    return render_template('Register.html');
