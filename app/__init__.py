ba9c2bc056e9f778680257c78dd571b8449de87f
RamB98 <59060161+RamB98@users.noreply.github.com>
2021-10-14 09:35:05 -0400

    added an answers mecanism frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, static_url_path='/static')
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///stackoverflow.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SECRET_KEY']='c7a6d8591c9bfb4a9914604b'
+db =SQLAlchemy(app)
+
+
+
+
+
+
+
+
+
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,32 @@
 from flask import render_template, url_for, flash, redirect, request, abort
+from flask_wtf import form
 from . import app
+from app.models import User
+from app import db
+import random
+value = random.randint(1,3000)
+
+
+
 
 @app.route("/")
 def home():
     return render_template('Home.html')
+
+
+@app.route("/post", methods=["POST","GET"])
+def post(): 
+    if request.method=="POST":
+       rolo= request.form["nm"]
+       guest="guest"+str(value)
+       input= User(username=guest, answer=rolo)
+       db.session.add(input)
+       db.session.commit()
+       return redirect("/post")
+       #return f"<h1>{rolo}</h1>"
+    else: milo=User.query.all()
+    return render_template('post.html',rolo=milo)
+
+@app.route("/<usr>")
+def rolo(usr):
+    return f"<h1>{usr}</h/>"
