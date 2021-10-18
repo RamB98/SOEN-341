6814f2861892c7ea9cb66c423d757430fa346f91
Mohammed Al-Taie <altaiem888@gmail.com>
2021-10-18 18:17:20 -0400

    created view question html - not complete
    Co-authored-by: paulwestenberg <paulwestenberg@users.noreply.github.com>

--- app/forms.py
+++ app/forms.py
@@ -5,33 +5,41 @@ from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationE
 from wtforms.widgets.core import TextArea
 from app.models import User
 
+
 class RegisterForm(FlaskForm):
 
     def validate_username(self, potential_username):
         user = User.query.filter_by(username=potential_username.data).first()
         if user:
-            raise ValidationError('Username is already in use, please try another!')
+            raise ValidationError(
+                'Username is already in use, please try another!')
 
     def validate_email(self, potential_email):
         em = User.query.filter_by(email=potential_email.data).first()
         if em:
-            raise ValidationError('Email is already in use, please try another!')
+            raise ValidationError(
+                'Email is already in use, please try another!')
 
-    username = StringField(label='User Name:', validators=[Length(min=2, max=80)])
-    email = StringField(label='Email Address:', validators=[Email(), DataRequired()])
-    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
-    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
+    username = StringField(label='User Name:', validators=[
+                           Length(min=2, max=80)])
+    email = StringField(label='Email Address:', validators=[
+                        Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[
+                              Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[
+                              EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
+
 
 class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
     password = PasswordField(label='Password:', validators=[DataRequired()])
     submit = SubmitField(label='Sign in')
 
+
 class PostForm(FlaskForm):
     title = StringField(label='Title ', validators=[DataRequired()])
-    question = TextAreaField('Enter your question here: ', widget=TextArea(), validators=[DataRequired()])    
-    tags= StringField(label='Tags: ')
+    question = TextAreaField('Enter your question here: ',
+                             widget=TextArea(), validators=[DataRequired()])
+    tags = StringField(label='Tags: ')
     submit = SubmitField(label='Create Post')
-
-
--- app/routes.py
+++ app/routes.py
@@ -9,59 +9,67 @@ from app import db
 import random
 
 
-
-
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('Home.html')
 
+
 @app.route("/login", methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
     if form.validate_on_submit():
-        attempted_user = User.query.filter_by(username=form.username.data).first()
+        attempted_user = User.query.filter_by(
+            username=form.username.data).first()
         if attempted_user and attempted_user.check_correct_password(attempted_password=form.password.data):
             login_user(attempted_user)
-            flash(f'Success! You are logged in as: { attempted_user.username }', category='success')
+            flash(
+                f'Success! You are logged in as: { attempted_user.username }', category='success')
             return redirect(url_for('home'))
         else:
-            flash('Username and password do not match, please try again!', category='danger')
+            flash('Username and password do not match, please try again!',
+                  category='danger')
     return render_template('Login.html', form=form)
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        new_user = User(username=form.username.data, email=form.email.data, password=form.password1.data)
+        new_user = User(username=form.username.data,
+                        email=form.email.data, password=form.password1.data)
         db.session.add(new_user)
         db.session.commit()
-        flash(f'Success! You have registered a new user: { new_user.username }', category='success')
+        flash(
+            f'Success! You have registered a new user: { new_user.username }', category='success')
         return redirect(url_for('home'))
     if form.errors != {}:
         for err_msg in form.errors.values():
             flash(f'User Creation Error: {err_msg}', category='danger')
     return render_template('Register.html', form=form)
 
-@app.route("/post", methods=["POST","GET"])
+
+@app.route("/post", methods=["POST", "GET"])
 @login_required
-def post(): 
+def post():
     form = PostForm()
     if form.validate_on_submit():
-        loggedin=flask_login.current_user
-        new_question = Question(title=form.title.data, question=form.question.data, username=loggedin.username)
+        loggedin = flask_login.current_user
+        new_question = Question(
+            title=form.title.data, question=form.question.data, username=loggedin.username)
         db.session.add(new_question)
         db.session.commit()
-        flash(f'Success! Your question { new_question.title } has been created!', category='success')
+        flash(
+            f'Success! Your question { new_question.title } has been created!', category='success')
         return redirect(url_for('forum_page'))
-    else: 
+    else:
         for err_msg in form.errors.values():
-            flash(f'User Creation Error: {err_msg}', category='danger') 
+            flash(f'User Creation Error: {err_msg}', category='danger')
     return render_template('post.html', form=form)
 
     # if request.method=="POST":
     #    #value = random.randint(1,30000)
-    
+
     #    respons= request.form["nm"]
     #    guest=flask_login.current_user
     #    input1= Question(username=guest.username, question=respons)
@@ -71,11 +79,13 @@ def post():
 
     # else: question=Question.query.all()
 
+
 @app.route("/logout")
 def logout_page():
     logout_user()
     flash("You have been logged out!", category='info')
     return redirect(url_for("home"))
+
 
 @app.route('/forum')
 def forum_page():
@@ -84,3 +94,7 @@ def forum_page():
     return render_template('Forum.html', allquestions=questions)
 
 
+@app.route('/viewquestion')
+def viewquestion_page():
+    questions = Question.query.all()
+    return render_template('ViewQuestion.html', allquestions=questions)
